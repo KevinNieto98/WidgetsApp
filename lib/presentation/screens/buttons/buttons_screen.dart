dfd7a572cb34dd4cb6d6345fb02303c56d130164
Kevin Nieto <155347741+KevinNieto98@users.noreply.github.com>
2024-08-08 21:31:19 -0600

    Pantallas creadas

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -76,6 +76,8 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon(Icons.add_link),
               label: const Text('TextButton Icon'),
             ),
+
+            const CustomButton(),
             IconButton(
               onPressed: (){}, 
               icon: const Icon(Icons.app_registration)
@@ -92,6 +94,34 @@ class _ButtonsView extends StatelessWidget {
             ),
           ],
         )
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;    
+
+    return  ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: const Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child:  Text(  
+              "Custom Button",
+              style: TextStyle(
+                color: Colors.white,
+          
+              ),
+            )),
+        ),
       ),
     );
   }
